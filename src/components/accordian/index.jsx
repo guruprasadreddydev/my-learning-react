4718a275e8d785e0557020212406fa66ccd829f6
Guru Prasad Reddy <guruprasadreddy2000@gmail.com>
2024-01-03 17:31:01 +0530

    add - and + feature

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -3,12 +3,11 @@ import data from "./data";
 
 const Accordian = () => {
   const [selected, setSelected] = useState(null);
-  const [isMultiSelection, setIsMultiSelection] = useState(false);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
   const [multiple, setMultiple] = useState([]);
 
   function handleSingleSelection(getCurrentId) {
     setSelected(getCurrentId === selected ? null : getCurrentId);
-    console.log(getCurrentId);
   }
 
   function handleMultiSelection(getCurrentId) {
@@ -18,19 +17,17 @@ const Accordian = () => {
     if (findIndexOfCurrentId === -1) copyMultiple.push(getCurrentId);
     else copyMultiple.splice(findIndexOfCurrentId, 1);
 
-    console.log(multiple);
     setMultiple(copyMultiple);
   }
-  console.log(selected, multiple);
 
   return (
     <div className="flex justify-center h-[100vh] w-[100vw] items-center flex-col">
       <button
-        onClick={() => setIsMultiSelection(!isMultiSelection)}
-        className="mb-3 px-4 py-2 rounded-full text-white bg-slate-500 hover:bg-slate-700 focus:outline-none focus:ring focus:border-slate-300"
+        onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+        className="px-5 py-3  m-5 rounded-full text-white bg-slate-500 hover:bg-slate-700 focus:outline-none focus:ring focus:bg-slate-600"
       >
-        {isMultiSelection
-          ? "Disabled Multi Selection"
+        {enableMultiSelection
+          ? "Disable Multi Selection"
           : "Enable Multi Selection"}
       </button>
       <div className="w-[500px]">
@@ -39,7 +36,7 @@ const Accordian = () => {
             <div className="bg-slate-500 mb-2 p-5 cursor-pointer">
               <div
                 onClick={
-                  isMultiSelection
+                  enableMultiSelection
                     ? () => handleMultiSelection(dataItem.id)
                     : () => handleSingleSelection(dataItem.id)
                 }
@@ -52,11 +49,18 @@ const Accordian = () => {
                 justify-between 
                 "
               >
-                <h3>{dataItem.question}</h3>
-                <span>{selected === dataItem.id ? "-" : "+"}</span>
+                <h1>{dataItem.question}</h1>
+                <span>
+                  {enableMultiSelection
+                    ? multiple.indexOf(dataItem.id) !== -1
+                      ? "–"
+                      : "+"
+                    : selected === dataItem.id
+                    ? "–"
+                    : "+"}
+                </span>
               </div>
-
-              {isMultiSelection
+              {enableMultiSelection
                 ? multiple.indexOf(dataItem.id) !== -1 && (
                     <div className="p-1 m-1">{dataItem.answer}</div>
                   )
